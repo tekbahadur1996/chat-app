ee863c70ec3f9be438dac88ed88d02fc6a39f129
Tek Bahadur Thapa <tekbahadur1996@gmail.com>
2017-06-23 18:14:16 +0530

    Add css

--- public/js/index.js
+++ public/js/index.js
@@ -48,11 +48,12 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+
   socket.emit('createMessage', {
-    from: 'user',
+    from: 'User',
     text: jQuery('[name=message]').val()
   }, function () {
-
+    jQuery('[name=message]').val('');
   });
 });
 
@@ -62,13 +63,16 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location');
   });
 });
--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ io.on('connection', (socket) => {
     console.log('createMessage', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('this is from the server');
+    callback();
     // socket.broadcast.emit(('newMessage'), {
     //   from: message.from,
     //   text: message.text,
