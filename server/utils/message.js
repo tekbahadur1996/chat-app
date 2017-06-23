44756a20a3c4ab431f8541e6538aeeae0a3d92be
Tek Bahadur Thapa <tekbahadur1996@gmail.com>
2017-06-23 17:36:58 +0530

    Add geolocation support via geolocation api

--- server/utils/message.js
+++ server/utils/message.js
@@ -5,5 +5,12 @@ var generateMessage = (from,text) => {
     createAt: new Date().getTime()
   }
 };
+var generateLocationMessage = (from, latitude, longitude)=> {
+  return {
+    from,
+    url:`https://www.google.com/maps?q=${latitude},${longitude}`,
+    createAt: new Date().getTime()
+  };
+};
 
-module.exports = {generateMessage};
+module.exports = {generateMessage, generateLocationMessage};
